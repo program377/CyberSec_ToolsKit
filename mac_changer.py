c4c8026b856223ec66b1e5bd9f7cac2cf90a19aa
program377 <program85@protonmail.com>
2025-06-13 10:50:57 -0400

    Version 0.1

--- mac_changer.py
+++ mac_changer.py
@@ -2,11 +2,12 @@ import scapy
 import subprocess
 import argparse
 
-
-def get_cmd():
+def get_mac():
     cmd = ['ip', 'add', 'show']
     cmd_out= subprocess.run(cmd, capture_output=True)
     print(cmd_out)
 
 
-parse= argparse.ArgumentParser(description="Get the MAC address of ")
+parser= argparse.ArgumentParser(description="Get the MAC address of any interfaces")
+parser.add_argument('-i', '--interface', required=True, help='Specify the interface')
+args = parser.parse_args()
