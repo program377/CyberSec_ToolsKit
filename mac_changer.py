1e345c86a9a43b371bdb09fb987d5f4370d50326
program377 <program85@protonmail.com>
2025-06-20 11:32:36 -0400

    Version 0.1

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,25 @@
 import scapy
 import subprocess
+import re
 
 def get_mac(interface = None):
-    if interface:
-        cmd = ['ip', 'link', 'show', interface]
-    else:
-        cmd = ['ip', 'link', 'show']
-    cmd_out= subprocess.run(cmd, capture_output=True, text=True)
-    print(cmd_out.stdout)
 
+
+        if interface:
+            try:
+                output = subprocess.check_output(['ip', 'link', 'show', interface], text=True)
+                mac = re.search(r"link/ether ([0-9a-f:]{17})")
+                if mac:
+                    print(f"{interface} => {mac.group(1)}")
+                else:
+                    print(f"[!] MAC address for {interface} not found [!]")
+            except subprocess.CalledProcessError:
+                print(f"[!] Interface {interface} not found [!]")
+        else:
+            try:
+                output = subprocess.check_output(['ip', 'link', 'show'], text=True)
+                mac = re.search()
+                print(f"")
+            except subprocess.CalledProcessError:
+                print("[!] Error has been encountered [!]")
 
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from mac_changer import get_mac
 
 def main():
     parser = argparse.ArgumentParser(description="Get the MAC address of any interfaces")
-    parser.add_argument('-all', action='store_true', help='Display all interfaces MAC')
-    parser.add_argument('-i', '--interface', dest='interface', required=True, help='Specify the interface')
+    group =parser.add_mutually_exclusive_group(required=True)  #Create a group of exclusive args to use one of them
+    group.add_argument('-all', action='store_true', help='Display all interfaces MAC')
+    group.add_argument('-i', '--interface', dest='interface', help='Specify the interface')
     args = parser.parse_args()
     if args.all:
         print("[+] Showing all interfaces MACs [+]")
@@ -13,7 +14,7 @@ def main():
         print(f"[+] Geting MAC of interface {args.interaface} [+]")
         get_mac(args.interface)
     else:
-        print("Device {args.interface} does not exist")
+        print("[+] Device {args.interface} does not exist [+]")
 
 
 
