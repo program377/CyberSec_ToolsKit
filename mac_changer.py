14d7cab5e146da0330cc4e026e2a03978ee2332a
program377 <program85@protonmail.com>
2025-06-26 05:53:20 -0400

    Version 0.1

--- mac_changer.py
+++ mac_changer.py
@@ -6,7 +6,7 @@ def get_mac():
     """Returns a dictionary {interface: mac_address}"""
     mac_iface_dict = {}
     try:
-        output = subprocess.check_output(['ip', '-o', 'link'], text=True)
+        output = subprocess.check_output(['ip', '-o', 'link'], text=True) # This command display both the interface & mac on the same line
         pattern = re.compile(r"^\d+: ([\w@.\-]+):.*link/ether ([0-9a-f:]{17})", re.MULTILINE)
 
         for line in output.splitlines():
@@ -15,10 +15,8 @@ def get_mac():
                 iface = match.group(1)
                 mac = match.group(2)
                 mac_iface_dict[iface] = mac
-
     except subprocess.CalledProcessError:
         raise RuntimeError("Failed to retrieve interface information.")
-
     return mac_iface_dict
 
 def manual_mac(ifaces):
