7b22969d4436ce99441fb8443346acc988c8861a
program377 <program85@protonmail.com>
2025-08-15 23:55:50 +0000

    0.1

--- main.py
+++ main.py
@@ -8,24 +8,27 @@ def main():
     parser.add_argument('--all', action='store_true', help='Display all interfaces MACs')
     parser.add_argument('-i', '--interface', dest='interface', help='Specify the interface name')
     parser.add_argument('-m', '--manual', dest='manual', help='Manually change the MAC address. Root access needed')
-    parser.add_argument('-a','--auto', dest='auto', help='Automatically change the MAC address')
+    parser.add_argument('-a','--auto', action='store_true', help='Automatically change the MAC address')
     args = parser.parse_args()
     ifaces_macs = get_mac()
-    if args.interface not in ifaces_macs.keys() or (args.manual and not args.interface):
-        ifaces_checking(args.interface, ifaces_macs)
-        sys.exit(1)
-    if args.manual and args.interface:
-        manual_mac(args.interface, args.manual)
+
     if args.all:
         print("[+] Showing all interfaces MACs [+]")
         ifaces_macs = get_mac()
         for iface, mac in ifaces_macs.items():
             print(f"Interface: {iface} -> MAC: {mac}")
+        exit(0)
+    if args.interface not in ifaces_macs.keys() or (args.manual and not args.interface):
+        ifaces_checking(args.interface, ifaces_macs)
+        sys.exit(1)
+    if args.manual and args.interface:
+        manual_mac(args.interface, args.manual)
+
     if args.interface in ifaces_macs:
         print(f"[+] Getting MAC of interface {args.interface} [+]")
         print(f"{args.interface} => {ifaces_macs[args.interface]}")
 
-    if args.auto:
+    if args.auto and args.interface:
         first_half =_1st_half_mac()
         sec_half = _2nd_half_mac
         auto_mac(first_half, sec_half)
