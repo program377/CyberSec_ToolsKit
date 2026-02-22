7062b17510b303d3d1508e5c7192b9df501d4cac
program377 <program85@protonmail.com>
2026-02-22 13:27:28 +0000

    2.3

--- vulnerabilities_assessments/cve_matching.py
+++ vulnerabilities_assessments/cve_matching.py
@@ -1,5 +1,6 @@
 from unittest.loader import VALID_MODULE_NAME
 
+from numpy import append
 import requests
 import os
 from dotenv import load_dotenv
@@ -19,27 +20,11 @@ def query_nvd(service, version):
     response = requests.get(url, params=params, headers=headers)
     if response.status_code != 200:
         return []
-    
+
     data = response.json()
     vulnerabilities = []
     for item in data.get("vulnerabilities", []):
-        cve_item = item["cve"]  # full dictionary
-        cve_id = cve_item['id']
-
-        # Default values in case CVSS data is missing
-        base_score = "N/A"
-        severity = "N/A"
-
-        metrics = cve_item.get('metrics', {})
-        if 'cvssMetricV2' in metrics:
-            cvss = metrics['cvssMetricV2'][0]
-            base_score = cvss['cvssData']['baseScore']
-            severity = cvss.get('baseSeverity', 'N/A')
-        elif 'cvssMetricV3' in metrics:
-            cvss = metrics['cvssMetricV3'][0]
-            base_score = cvss['cvssData']['baseScore']
-            severity = cvss.get('baseSeverity', 'N/A')
-
-    print(f"{cve_id} (CVSS {base_score} – {severity})")
-    
+        cve = item["cve"]["id"]
+        vulnerabilities.append(cve)
+        
     return vulnerabilities
